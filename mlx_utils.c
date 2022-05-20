14204cc0077d755e8e2a9f73c809d76ae9c1238b
shwatanap <watanabeshou1520@gmail.com>
2022-05-20 21:08:24 +0900

    :memo: #1 fix norm

--- mlx_utils.c
+++ mlx_utils.c
@@ -23,5 +23,5 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 uint32_t	get_color(t_img img, int x, int y)
 {
 	return (*(uint32_t *)(img.addr + (y * img.line_length + x
-				* (img.bits_per_pixel / 8))));
+			* (img.bits_per_pixel / 8))));
 }
