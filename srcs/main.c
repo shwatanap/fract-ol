103b3f1ae99bcee305c4f66ba21eb14e5b4780ff
shwatanap <watanabeshou1520@gmail.com>
2022-05-21 18:00:11 +0900

    :sparkles: #9 add select fractal type by arg

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 21:04:04 by shwatana          #+#    #+#             */
-/*   Updated: 2022/05/21 17:53:05 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/05/21 17:58:45 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,16 @@ void	update_fractal_c(t_canvas *canvas)
 	c_info->c.im = mouse_im;
 }
 
-// plot_mandelbrot(canvas);
 int	main_loop(t_canvas *canvas)
 {
 	if (canvas->is_pressed_mouse_left)
 		update_fractal_c(canvas);
-	plot_julia(canvas);
+	if (canvas->fractal_type == 0)
+		plot_mandelbrot(canvas);
+	else if (canvas->fractal_type == 1)
+		plot_julia(canvas);
+	else
+		exit(0);
 	mlx_put_image_to_window(canvas->mlx, canvas->win, canvas->img.img, 0, 0);
 	return (0);
 }
@@ -49,7 +53,7 @@ int	main(int argc, char **argv)
 	if (argc != 2 || !(argv[1][0] == '0' || argv[1][0] == '1'))
 	{
 		ft_putstr_fd(INVALID_ARG_MSG, STDOUT_FILENO);
-		exit(1);
+		return (1);
 	}
 	canvas.fractal_type = argv[1][0] - '0';
 	init_canvas(&canvas);
