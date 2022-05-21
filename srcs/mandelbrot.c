726192b80a58a4eb9e9a51418ab0e7bf2ee50309
shwatanap <watanabeshou1520@gmail.com>
2022-05-21 14:05:09 +0900

    :memo: #4 rename c_num to c_info

--- srcs/mandelbrot.c
+++ srcs/mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 21:04:09 by shwatana          #+#    #+#             */
-/*   Updated: 2022/05/21 14:01:31 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/05/21 14:04:31 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ static uint32_t	get_color_in_fractal(t_canvas *canvas)
 	int				iteration;
 	int				color;
 	double			tmp_x;
-	t_complex_info	c_num;
+	t_complex_info	c_info;
 
 	iteration = 0;
-	c_num = canvas->comp_num;
-	while (squared(c_num.z.re) + squared(c_num.z.im) <= 4)
+	c_info = canvas->comp_num;
+	while (squared(c_info.z.re) + squared(c_info.z.im) <= 4)
 	{
-		tmp_x = squared(c_num.z.re) - squared(c_num.z.im) + c_num.c.re;
-		c_num.z.im = 2 * c_num.z.re * c_num.z.im + c_num.c.im;
-		c_num.z.re = tmp_x;
+		tmp_x = squared(c_info.z.re) - squared(c_info.z.im) + c_info.c.re;
+		c_info.z.im = 2 * c_info.z.re * c_info.z.im + c_info.c.im;
+		c_info.z.re = tmp_x;
 		iteration++;
 		if (iteration == canvas->max_iter)
 			return (rgb2hex(0, 0, 0));
@@ -41,21 +41,21 @@ void	plot_mandelbrot(t_canvas *canvas)
 {
 	int				x;
 	int				y;
-	t_complex_info	*c_num;
+	t_complex_info	*c_info;
 
-	c_num = &canvas->comp_num;
-	c_num->delta.re = (c_num->max.re - c_num->min.re) / WIDTH;
-	c_num->delta.im = (c_num->max.im - c_num->min.im) / HEIGHT;
+	c_info = &canvas->comp_num;
+	c_info->delta.re = (c_info->max.re - c_info->min.re) / WIDTH;
+	c_info->delta.im = (c_info->max.im - c_info->min.im) / HEIGHT;
 	y = 0;
 	while (y < HEIGHT)
 	{
 		x = 0;
 		while (x < WIDTH)
 		{
-			c_num->z.re = 0;
-			c_num->z.im = 0;
-			c_num->c.im = c_num->min.im + y * c_num->delta.im;
-			c_num->c.re = c_num->min.re + x * c_num->delta.re;
+			c_info->z.re = 0;
+			c_info->z.im = 0;
+			c_info->c.im = c_info->min.im + y * c_info->delta.im;
+			c_info->c.re = c_info->min.re + x * c_info->delta.re;
 			my_mlx_pixel_put(&canvas->img, x, y, get_color_in_fractal(canvas));
 			x++;
 		}
