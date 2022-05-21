8bb2e27f373aabee2b3f2ada158b62530c18e8be
shwatanap <watanabeshou1520@gmail.com>
2022-05-21 23:30:28 +0900

    :memo: #17 compile simular func

--- srcs/plot_fractal.c
+++ srcs/plot_fractal.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 21:04:09 by shwatana          #+#    #+#             */
-/*   Updated: 2022/05/21 22:57:11 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/05/21 23:28:51 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,13 @@ static uint32_t	get_fractal_color(t_canvas *canvas)
 	while (squared(c_info.z.re) + squared(c_info.z.im) <= 4)
 	{
 		tmp_x = squared(c_info.z.re) - squared(c_info.z.im) + c_info.c.re;
-		c_info.z.im = 2 * c_info.z.re * c_info.z.im + c_info.c.im;
+		if (canvas->fractal_type != '2')
+			c_info.z.im = 2 * c_info.z.re * c_info.z.im + c_info.c.im;
+		else
+		{
+			c_info.z.im = abs_double((double)2 * c_info.z.re * c_info.z.im)
+				+ c_info.c.im;
+		}
 		c_info.z.re = tmp_x;
 		iteration++;
 		if (iteration == canvas->max_iter)
@@ -32,51 +38,12 @@ static uint32_t	get_fractal_color(t_canvas *canvas)
 	return (simple_colorizer(iteration, canvas));
 }
 
-static uint32_t	get_burningship_color(t_canvas *canvas)
-{
-	int				iteration;
-	double			tmp_x;
-	t_complex_info	*c_info;
-
-	c_info = &canvas->comp_info;
-	iteration = 0;
-	while (c_info->z.re * c_info->z.re + c_info->z.im * c_info->z.im <= 4
-		&& iteration < canvas->max_iter)
-	{
-		tmp_x = c_info->z.re * c_info->z.re - c_info->z.im * c_info->z.im
-			+ c_info->c.re;
-		c_info->z.im = abs_double((double)2 * c_info->z.re * c_info->z.im)
-			+ c_info->c.im;
-		c_info->z.re = tmp_x;
-		iteration++;
-		if (iteration == canvas->max_iter)
-			return (convert_rgb_hex(0, 0, 0));
-	}
-	return (simple_colorizer(iteration, canvas));
-}
-
-static void	update_complex_num(t_canvas *canvas, int x, int y)
-{
-	t_complex_info	*c_info;
-
-	c_info = &canvas->comp_info;
-	c_info->z.re = 0;
-	c_info->z.im = 0;
-	c_info->c.im = c_info->min.im + y * c_info->delta.im;
-	c_info->c.re = c_info->min.re + x * c_info->delta.re;
-}
-
 void	plot_mandelbrot_and_burningship(t_canvas *canvas)
 {
-	int					x;
-	int					y;
-	t_complex_info		*c_info;
-	t_get_color_func	get_color_func;
+	int				x;
+	int				y;
+	t_complex_info	*c_info;
 
-	if (canvas->fractal_type == '0')
-		get_color_func = get_fractal_color;
-	else if (canvas->fractal_type == '2')
-		get_color_func = get_burningship_color;
 	c_info = &canvas->comp_info;
 	c_info->delta.re = (c_info->max.re - c_info->min.re) / WIN_WIDTH;
 	c_info->delta.im = (c_info->max.im - c_info->min.im) / WIN_HEIGHT;
@@ -86,8 +53,11 @@ void	plot_mandelbrot_and_burningship(t_canvas *canvas)
 		x = 0;
 		while (x < WIN_WIDTH)
 		{
-			update_complex_num(canvas, x, y);
-			my_mlx_pixel_put(&canvas->img, x, y, get_color_func(canvas));
+			c_info->z.re = 0;
+			c_info->z.im = 0;
+			c_info->c.im = c_info->min.im + y * c_info->delta.im;
+			c_info->c.re = c_info->min.re + x * c_info->delta.re;
+			my_mlx_pixel_put(&canvas->img, x, y, get_fractal_color(canvas));
 			x++;
 		}
 		y++;
