3434fbc9842f6085b1abfb970f17c65867e1deb9
shwatanap <watanabeshou1520@gmail.com>
2022-06-17 11:03:28 +0900

    :memo: fine tuning

--- srcs/mlx_mouse_hooks.c
+++ srcs/mlx_mouse_hooks.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 13:54:53 by shwatana          #+#    #+#             */
-/*   Updated: 2022/06/15 20:15:14 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/06/17 11:00:46 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,16 @@ static void	zoom_canvas(t_canvas *canvas, int button, int x, int y)
 	mouse_im = (double)y / (WIN_HEIGHT / (c_info->max.im - c_info->min.im))
 		+ c_info->min.im;
 	if (button == SCROLL_UP)
+	{
+		if (canvas->max_iter > 4)
+			canvas->max_iter -= 4;
 		magnification = 1.0 / 0.8;
+	}
 	else if (button == SCROLL_DOWN)
+	{
+		canvas->max_iter += 4;
 		magnification = 1.0 / 1.2;
+	}
 	c_info->min.re = magnify(mouse_re, c_info->min.re, magnification);
 	c_info->min.im = magnify(mouse_im, c_info->min.im, magnification);
 	c_info->max.re = magnify(mouse_re, c_info->max.re, magnification);
